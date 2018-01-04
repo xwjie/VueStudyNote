2a2d82cac1039d15c12c33f86d645f20336dbdd0
xwjie <121509092@qq.com>
2018-01-04 22:45:46 +0800

    引入htmlparser

--- Xiao/src/lifecycle.js
+++ Xiao/src/lifecycle.js
@@ -1,17 +1,41 @@
 /* @flow */
-// D:\OutPut\VUE\vue\src\core\instance\lifecycle.js
 import Xiao from './main'
+import { warn, log } from './util/debug'
 
+import {HTMLParser, HTMLtoXML, HTMLtoDOM} from './compiler/htmlparser'
 
+// D:\OutPut\VUE\vue\src\core\instance\lifecycle.js
 export function mountComponent(
     vm: Xiao,
-    el: ?Element,
-    hydrating?: boolean    
-){}
-// export function mountComponent(
-//     vm: Xiao,
-//     el: ?Element,
-//     hydrating?: boolean
-// ): Xiao {
-//     vm.$el = el
-// }  
+    el: Element,
+    hydrating?: boolean
+) {
+    let results : string = '';
+    
+    const htmlString: string = "<p id=test>hello <i>world";
+
+    HTMLParser(htmlString, {
+        start: function (tag, attrs, unary) {
+            results += "<" + tag;
+
+            for (var i = 0; i < attrs.length; i++)
+                results += " " + attrs[i].name + '="' + attrs[i].escaped + '"';
+
+            results += (unary ? "/" : "") + ">";
+        },
+        end: function (tag) {
+            results += "</" + tag + ">";
+        },
+        chars: function (text) {
+            results += text;
+        },
+        comment: function (text) {
+            results += "<!--" + text + "-->";
+        }
+    });
+
+    log('htmlparser HTMLtoXML', HTMLtoXML(htmlString));
+    log('htmlparser HTMLtoDOM', HTMLtoDOM(htmlString));
+    log('htmlparser HTMLParser', results == '<p id="test">hello <i>world</i></p>');
+
+}
