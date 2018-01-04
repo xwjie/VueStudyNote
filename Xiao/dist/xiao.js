f7861cde02dbaa9127dbbdf16d41e4ac68479758
xwjie <121509092@qq.com>
2018-01-04 08:39:00 +0800

    使用es6 class写法

--- Xiao/dist/xiao.js
+++ Xiao/dist/xiao.js
@@ -23,23 +23,118 @@ var warn = noop;
   };
 }
 
+// D:\OutPut\VUE\vue\src\core\instance\lifecycle.js
+
+
+function mountComponent(vm, el, hydrating) {}
+// export function mountComponent(
+//     vm: Xiao,
+//     el: ?Element,
+//     hydrating?: boolean
+// ): Xiao {
+//     vm.$el = el
+// }
+
+// D:\OutPut\VUE\vue\src\platforms\web\util\index.js
+
+/**
+ * Query an element selector if it's not an element already.
+ */
+
+function query(el) {
+  return '';
+}
+
+// export function query (el: string | Element): Element {
+//     if (typeof el === 'string') {
+//       const selected = document.querySelector(el)
+//       if (!selected) {
+//         "development" !== 'production' && warn(
+//           'Cannot find element: ' + el
+//         )
+//         return document.createElement('div')
+//       }
+//       return selected
+//     } else {
+//       return el
+//     }
+//   }
+
 var classCallCheck = function (instance, Constructor) {
   if (!(instance instanceof Constructor)) {
     throw new TypeError("Cannot call a class as a function");
   }
 };
 
-// D:\OutPut\VUE\vue\src\core\instance\index.js
-
-var Xiao = function Xiao(options) {
-  classCallCheck(this, Xiao);
-
-  if ("development" !== 'production' && !(this instanceof Xiao)) {
-    warn('Xiao is a constructor and should be called with the `new` keyword');
+var createClass = function () {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
   }
 
-  console.log('main start', options);
-};
+  return function (Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+}();
+
+// D:\OutPut\VUE\vue\src\core\instance\index.js
+
+var uid = 0;
+
+//fixme
+var inBrowser = true;
+
+var Xiao = function () {
+  function Xiao(options) {
+    classCallCheck(this, Xiao);
+
+    if ("development" !== 'production' && !(this instanceof Xiao)) {
+      warn('Xiao is a constructor and should be called with the `new` keyword');
+    }
+
+    this.$options = options || {};
+
+    console.log('main start', options);
+  }
+
+  createClass(Xiao, [{
+    key: '$mount',
+    value: function $mount(el, // | Element
+    hydrating) {
+
+      el = el && inBrowser ? query(el) : ''; //fixme undefined
+      return mountComponent(this, el, hydrating);
+    }
+  }, {
+    key: '_init',
+    value: function _init(options) {
+      var vm = this;
+
+      if (vm.$options.el) {
+        vm.$mount(vm.$options.el);
+      }
+    }
+  }, {
+    key: '_uid',
+    get: function get$$1() {
+      return uid++;
+    }
+  }, {
+    key: '$options',
+    get: function get$$1() {
+      return {};
+    },
+    set: function set$$1(options) {}
+  }]);
+  return Xiao;
+}();
 
 return Xiao;
 
--- Xiao/src/main.js
+++ Xiao/src/main.js
@@ -2,17 +2,56 @@
 // D:\OutPut\VUE\vue\src\core\instance\index.js
 
 import { warn } from './util'
+import { mountComponent } from './lifecycle'
+import { query } from './util/web'
+
+let uid = 0;
+
+//fixme
+let inBrowser = true;
 
 class Xiao {
 
-  constructor(options: Object){
+  constructor(options: Object) {
     if (process.env.NODE_ENV !== 'production' &&
       !(this instanceof Xiao)
     ) {
       warn('Xiao is a constructor and should be called with the `new` keyword')
     }
 
+    this.$options = options || {};
+
     console.log('main start', options);
+  }
+
+  get _uid(): number {
+    return uid++;
+  }
+
+  get $options(): Object{
+    return {};
+  }
+
+  set $options(options: Object){
+
+  }
+
+  $mount(
+    el?: string,// | Element
+    hydrating?: boolean
+  ) {
+
+    el = el && inBrowser ? query(el) : '' //fixme undefined
+    return mountComponent(this, el, hydrating)
+  }
+
+  _init(options?: Object) {
+    const vm = this;
+
+
+    if (vm.$options.el) {
+      vm.$mount(vm.$options.el);
+    }
   }
 }
 
